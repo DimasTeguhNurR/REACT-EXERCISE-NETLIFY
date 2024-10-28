d42014af4a65d8d88341ed9b46c7c184d5044cb8
DimasTeguhNurR <samiddimas791@gmail.com>
2024-10-28 14:12:05 +0700

    Add files via upload

---
+++ Task.js
@@ -0,0 +1,5 @@
+const url = "https://dimasteguhnurrohmad-fs11642372.netlify.app/" 
+const studentName = "Dimas Teguh Nur Rohmad" 
+const studentId = "FS11642372" 
+
+export { url, studentName, studentId }
